28010f5c448cb778fb2205638a39ae7c77c46445
Manos Tsotros <6670043+immersionfx@users.noreply.github.com>
2024-07-18 16:17:41 +0300

    Update scrollbutton.js

--- js/scrollbutton.js
+++ js/scrollbutton.js
@@ -1,5 +1,4 @@
-function scrollTop() {
-    console.log($(window).scrollTop());    
+function scrollTop() { 
     if ($(window).scrollTop() > 500) { // 500 -> This is the value in px of the distance to be scrolled for the 'scroll-to-top' button to show-up
       $(".backToTopBtn").addClass("active");
     } else {
@@ -8,11 +7,9 @@ function scrollTop() {
   }
   
   $(document).ready( function() { 
-    // show and hide btn
     scrollTop();
     $(window).on("scroll", scrollTop);
-  
-    // body scroll on btn click
+
     $(".backToTopBtn").click(function () {
       $("html, body").animate({ scrollTop: 0 }, 1);
       return false;
